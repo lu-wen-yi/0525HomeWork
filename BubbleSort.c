bc977cb98119ca36a8bffaffc9422481f5c680fa
LuWenYi <loveqq0409@gmail.com>
2018-05-25 11:07:40 +0800

    指向非常數資料的非常數指標

---
+++ BubbleSort.c
@@ -0,0 +1,27 @@
+	#define SIZE 10
+	/*short an array of intergers using bubble sort algorithm*/
+	void bubbleSort(int * array, unsigned int size )
+	{
+		void swap(int *element1Ptr, int *element2Ptr );
+		unsigned int pass;
+		unsigned int j;
+	/*loop to control passes*/	
+	for (pass= 0;pass<SIZE;++pass){
+	    /*loop to control comparisons during each pass*/
+		for(j=0;j<size-1;++j){/*swap adjacent elements if theyre out of order*/
+				if(array[j]>array[j+1]){
+					swap(&array[j],&array[j+1]);		
+				}/*end if*/
+			}/*end inner for*/
+		}/*end outer for*/
+}/*end function bubbleSort*/
+
+/*swap values at memory locations to which 
+element1Ptr and element2Ptr*/
+
+void swap(int *element1Ptr,int *element2Ptr)
+{
+	int hold=*element1Ptr;
+	*element1Ptr=*element2Ptr;
+	*element2Ptr= hold;
+}/*end function swap*/
---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>  
+#include <stdlib.h>
+#define SIZE 10
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void bubbleSort(int * array,unsigned int size );/*prtotype*/ 
+
+int main(void) {
+	/*initalize array a*/
+	int a[SIZE]={36,27,18,6,79,40,81,70,17,43};
+	
+	unsigned int i; /*counter*/
+	
+	puts(" Data items in original order");
+	/*loop through array a*/
+		for(i=0;i<SIZE;++i){
+			printf("%4d ",a[i]);
+		} /*end for*/
+		
+		bubbleSort(a, SIZE);
+		
+		puts("\n Data items in ascending order");
+		
+		/*loop through array a*/
+		for(i=0;i<SIZE;++i){
+			printf("%4d ",a[i]);
+	    }/*end for*/
+		
+		puts("");
+	}/*end main*/ 
+
